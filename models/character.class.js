c1be345fc4f48632adc855b87b980ef23390b788
Martin Jaros <martinjaros88@gmail.com>
2024-10-19 20:28:16 +0200

    Implement character movement based on keyboard input

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     y = 120;
     height = 200;
     width = 240;
+    speed = 5;
     IMAGES_SWIMM = [
         'img/1.Sharkie/3.Swim/1.png',
         'img/1.Sharkie/3.Swim/2.png',
@@ -11,6 +12,7 @@ class Character extends MovableObject {
         'img/1.Sharkie/3.Swim/5.png',
         'img/1.Sharkie/3.Swim/6.png'
     ];
+    world;
 
     constructor() {
         super().loadImage('img/1.Sharkie/3.Swim/1.png');
@@ -21,12 +23,38 @@ class Character extends MovableObject {
 
 
     animate() {
+
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_SWIMM.length;
-            let path = this.IMAGES_SWIMM[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 100);
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+
+            if (this.world.keyboard.UP) {
+                this.y -= this.speed;
+            }
+
+            if (this.world.keyboard.DOWN) {
+                this.y += this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.DOWN) {
+
+                //Swimm animation
+                let i = this.currentImage % this.IMAGES_SWIMM.length;
+                let path = this.IMAGES_SWIMM[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 50);
     }
 
 
