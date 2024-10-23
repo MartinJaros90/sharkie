48e5059734b50c98d45e84e8e4730737b03f5474
Martin Jaros <martinjaros88@gmail.com>
2024-10-23 22:19:19 +0200

    code refactoring

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -28,6 +28,17 @@ class MovableObject{
 
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {   
+        ctx.beginPath();
+        ctx.lineWidth = '2';
+        ctx.strokeStyle = "red";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
 
     playAnimation(images){
         let i = this.currentImage % images.length;
